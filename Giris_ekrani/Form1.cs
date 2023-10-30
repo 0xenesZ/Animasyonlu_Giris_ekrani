7c0de5e2392e5bd28f3b5483266f992f18d4ef60
Enes <gunesenes034@gmail.com>
2023-10-30 12:53:19 +0300

    Add project files.

---
+++ Giris_ekrani/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giris_ekrani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool islem = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!islem)
+            {
+                this.Opacity += 0.009;
+            }
+            if (this.Opacity == 1.0)
+            {
+                islem = true;
+            }
+            if (islem)
+            {
+                this.Opacity -= 0.009;
+                if (this.Opacity == 0)
+                {
+                    Form2 gtr = new Form2();
+                    gtr.Show();
+                    timer1.Enabled = false;
+                }
+            }
+
+        }
+    }
+}
